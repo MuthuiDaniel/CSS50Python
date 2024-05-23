196594ad8596c7aad9e527bae1c06f27a5c3cfc8
MuthuiDaniel <wambua.daniel@strathmore.edu>
2024-05-24 01:14:00 +0300

    created integers and define functions

--- intro.py
+++ intro.py
@@ -1,13 +1,46 @@
-# print("hello world")
+# String
+print("hello world")
 # variable is container to store a value
 name = input("what's your name?").strip().title()
 # remove whitespace from str and capitalize user name
-# name = name.strip().title()
-# # capitalize user name
-# name = name.title()
+name = name.strip().title()
+# capitalize user name
+name = name.title()
+# split username into first name and last name
+first, last = name.split(" ")
+print(f"hello, {first} nice to meet you")
+age = input("what is your age\n")
+print(f"your age is {age}")
 
-print(f"hello, {name} nice to meet you")
-# age = input("what is your age\n")
-# print(f"your age is {age}")
-print("you are awesome")
+# ----------------------------------------------------------------
+# integer
+# +, - , *, /, %
+x = int(input("what's X?"))
+y = int(input("what's Y?"))
+print(x + y)
+# float - number with a decimal point
+x = float(input("what's X?"))
+y = float(input("what's Y?"))
+print(round(x + y))
+# division
+x = float(input("what's X?"))
+y = float(input("what's Y?"))
+z = round(x / y, 2)
+print(f"{z:.2f}")
+# ---------------------------------------------------------------------
+# def - functions/ define
+def main():
+    name = input("what's your name?")
+    return name
+main()
+
+def main():
+    x = int(input("what's x?"))
+    print("x squared is", square(x))
+
+def square(n):
+    return pow(n, 2)
+
+main()
+
 
