5cdae4129c30024a18455a435326e55c6b755fab
MuthuiDaniel <wambua.daniel@strathmore.edu>
2024-05-24 00:09:39 +0300

    string functions--strip,capitalize&title

---
+++ intro.py
@@ -0,0 +1,13 @@
+# print("hello world")
+# variable is container to store a value
+name = input("what's your name?").strip().title()
+# remove whitespace from str and capitalize user name
+# name = name.strip().title()
+# # capitalize user name
+# name = name.title()
+
+print(f"hello, {name} nice to meet you")
+# age = input("what is your age\n")
+# print(f"your age is {age}")
+print("you are awesome")
+
